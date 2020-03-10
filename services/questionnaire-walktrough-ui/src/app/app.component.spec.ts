557cbc8e31330b0fa7fd1dc1c0d602887963e2f5
suenodk <s.dekort@student.fontys.nl>
2020-03-10 14:36:06 +0100

    yarn toegevoegd, prettierrc toegevoegd aan de frontend services, node module packages worden hergebruikt in de root met behulp van yarn

--- services/questionnaire-walktrough-ui/src/app/app.component.spec.ts
+++ services/questionnaire-walktrough-ui/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'questionnaire-ui'`, () => {
+  it(`should have as title 'questionnaire-walktrough-ui'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('questionnaire-ui');
+    expect(app.title).toEqual('questionnaire-walktrough-ui');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('questionnaire-ui app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('questionnaire-walktrough-ui app is running!');
   });
 });
