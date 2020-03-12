6973378b8f58579028ce317e734077f03e71a331
Michiel Langen <michiel.langen@kpnmail.nl>
2020-03-12 10:04:47 +0100

    chore(questionnaireApi): added Models folder, added questionnaire model

---
+++ services/questionnaire-api/API/Models/questionnaire.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace API.Models
+{
+
+    public class Questionnaire
+    {
+
+        public int ID { get; set; }
+
+        public Questionnaire(int Id)
+        {
+            ID = Id;
+        }
+
+        //TODO: add parameters
+        //TODO: Create all Models
+    }
+}
