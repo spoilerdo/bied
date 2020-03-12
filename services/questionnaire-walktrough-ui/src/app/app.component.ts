9b1f7a7d9a7933491d994992fcc60f7cb8754b1c
suenodk <s.dekort@student.fontys.nl>
2020-03-12 09:50:24 +0100

    added nebular styles to research-ui

--- services/questionnaire-walktrough-ui/src/app/app.component.ts
+++ services/questionnaire-walktrough-ui/src/app/app.component.ts
@@ -1,9 +1,41 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  template: `
+    <nb-layout>
+      <nb-layout-header fixed>Company Name</nb-layout-header>
+
+      <nb-sidebar>Sidebar Content</nb-sidebar>
+
+      <nb-layout-column>
+        Page Content <button nbButton>Hello World</button>
+
+        <nb-reveal-card>
+          <nb-card-front>
+            <nb-card accent="success">
+              <nb-card-header>Front Card Header</nb-card-header>
+              <nb-card-body>
+                A nebula is an interstellar cloud of dust, hydrogen, helium and other ionized gases. Originally, nebula
+                was a name for any diffuse astronomical object, including galaxies beyond the Milky Way.
+              </nb-card-body>
+            </nb-card>
+          </nb-card-front>
+          <nb-card-back>
+            <nb-card accent="primary">
+              <nb-card-header>Back Card Header</nb-card-header>
+              <nb-card-body>
+                A nebula is an interstellar cloud of dust, hydrogen, helium and other ionized gases. Originally, nebula
+                was a name for any diffuse astronomical object, including galaxies beyond the Milky Way.
+              </nb-card-body>
+            </nb-card>
+          </nb-card-back>
+        </nb-reveal-card>
+      </nb-layout-column>
+    </nb-layout>
+  `,
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'questionnaire-walktrough-ui';
--- services/questionnaire-walktrough-ui/src/app/app.module.ts
+++ services/questionnaire-walktrough-ui/src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { NbThemeModule, NbLayoutModule, NbSidebarModule, NbButtonModule, NbCardModule } from '@nebular/theme';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NbThemeModule.forRoot(),
+    NbLayoutModule,
+    NbSidebarModule.forRoot(),
+    NbButtonModule,
+    NbCardModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
