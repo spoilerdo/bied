649d6bcc383e46a45d6e5a3a62336f8f0f0056af
Tygo Driessen <tygo.driessen@student.fontys.nl>
2020-03-16 13:31:10 +0100

    Nebular fix + action bar

--- services/research-ui/src/app/app.module.ts
+++ services/research-ui/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
 
 import { NbThemeModule, NbLayoutModule, NbButtonModule, NbCardModule } from '@nebular/theme';
 
-const nebularModules = [NbThemeModule.forRoot(), NbLayoutModule, NbButtonModule, NbCardModule];
+const nebularModules = [NbThemeModule.forRoot(), NbLayoutModule, NbButtonModule, NbCardModule, NbEvaIconsModule];
 
 @NgModule({
   declarations: [AppComponent],
--- services/research-ui/src/app/questionnaire/questionnaire.module.ts
+++ services/research-ui/src/app/questionnaire/questionnaire.module.ts
@@ -3,11 +3,18 @@ import { CommonModule } from '@angular/common';
 
 import { QuestionnaireRoutingModule } from './questionnaire-routing.module';
 import { QuestionnaireComponent } from './questionnaire.component';
-import {NbCardModule, NbInputModule, NbLayoutModule, NbSelectModule} from '@nebular/theme';
+import {
+  NbButtonModule,
+  NbCardModule,
+  NbIconModule,
+  NbInputModule,
+  NbLayoutModule,
+  NbSelectModule
+} from '@nebular/theme';
 import { CreateQuestionnaireComponent } from './pages/create-questionnaire/create-questionnaire.component';
 
 @NgModule({
   declarations: [QuestionnaireComponent, CreateQuestionnaireComponent],
-  imports: [CommonModule, QuestionnaireRoutingModule, NbLayoutModule, NbCardModule, NbInputModule, NbSelectModule],
+  imports: [CommonModule, QuestionnaireRoutingModule, NbLayoutModule, NbCardModule, NbInputModule, NbSelectModule, NbButtonModule, NbIconModule],
 })
 export class QuestionnaireModule {}
