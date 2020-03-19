d86e16d01a0ebfd340e6de4efa01d56c0793d534
nick <n>
2020-03-19 13:02:00 +0100

    Setup research service with GRPC, EF core, automapper.

---
+++ services/research-api/API/research_service/research_service/Mapping/ResearchProfile.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using research_service.Persistence.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace research_service.Mapping
+{
+    public class ResearchProfile : Profile
+    {
+        public ResearchProfile()
+        {
+            CreateMap<ResearchEntity, Research>()
+                .ForMember(dest => dest.StartDate, opt => opt.MapFrom(src => DateTimeToUnixTime(src.StartDate)))
+                .ForMember(dest => dest.EndDate, opt => opt.MapFrom(src => DateTimeToUnixTime(src.EndDate)));
+            CreateMap<ResearchCreateRequest, ResearchEntity>()
+                .ForMember(dest => dest.StartDate, opt => opt.MapFrom(src => UnixTimeToDateTime(src.StartDate)))
+                .ForMember(dest => dest.EndDate, opt => opt.MapFrom(src => UnixTimeToDateTime(src.EndDate)));
+            CreateMap<ResearchEditRequest, ResearchEntity>()
+                .ForMember(dest => dest.StartDate, opt => opt.MapFrom(src => UnixTimeToDateTime(src.StartDate)))
+                .ForMember(dest => dest.EndDate, opt => opt.MapFrom(src => UnixTimeToDateTime(src.EndDate)));
+
+        }
+
+        private DateTime UnixTimeToDateTime(long unixTime)
+        {
+            DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
+            dtDateTime = dtDateTime.AddMilliseconds(unixTime).ToLocalTime();
+            return dtDateTime;
+        }
+
+        private long DateTimeToUnixTime(DateTime dateTime)
+        {
+            long unixTime = ((DateTimeOffset)dateTime).ToUnixTimeSeconds();
+            return unixTime;
+        }
+    }
+}
---
+++ services/research-api/API/research_service/research_service/Services/ResearchService.cs
@@ -0,0 +1,91 @@
+﻿using AutoMapper;
+using Grpc.Core;
+using research_service.Persistence.Entities;
+using research_service.Persistence.Repositories.Researches;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace research_service.Services
+{
+    public class Research_Service : ResearchService.ResearchServiceBase
+    {
+        private readonly IResearchRepository _researchRepository;
+        private readonly IMapper _mapper;
+
+        public Research_Service(IResearchRepository researchRepository, IMapper mapper)
+        {
+            _researchRepository = researchRepository;
+            _mapper = mapper;
+        }
+
+        public override async Task<Research> CreateResearch(ResearchCreateRequest request, ServerCallContext context)
+        {
+            var createdResearch = await _researchRepository.CreateResearch(_mapper.Map<ResearchEntity>(request));
+            if(!createdResearch.Success)
+            {
+                throw new NotImplementedException("Error handling for GRPC not implemented yet");
+            }
+            return _mapper.Map<Research>(createdResearch.Data); 
+        }
+
+        public override async Task<ResearchEmptyResponse> DeleteResearch(ResearchIdRequest request, ServerCallContext context)
+        {
+            var deletedResearch = await _researchRepository.DeleteResearch(new Guid(request.Id));
+            if(!deletedResearch.Success)
+            {
+                throw new NotImplementedException("Error handling for GRPC not implemented yet");
+            }
+            return new ResearchEmptyResponse();
+        }
+
+        public override async Task<Research> EditResearch(ResearchEditRequest request, ServerCallContext context)
+        {
+            var editedResearch = await _researchRepository.UpdateResearch(new Guid(request.Id), _mapper.Map<ResearchEntity>(request));
+            if(!editedResearch.Success)
+            {
+                throw new NotImplementedException("Error handling for GRPC not implemented yet");
+            }
+            return _mapper.Map<Research>(editedResearch.Data);
+        }
+
+        public override async Task<Research> GetResearch(ResearchIdRequest request, ServerCallContext context)
+        {
+            var foundResearch = await _researchRepository.GetResearchById(new Guid(request.Id));
+            if(!foundResearch.Success)
+            {
+                throw new NotImplementedException("Error handling for GRPC not implemented yet");
+            }
+            return _mapper.Map<Research>(foundResearch.Data);
+        }
+
+        public override async Task<Researches> GetResearches(GetResearchesRequest request, ServerCallContext context)
+        {
+            var researches = await _researchRepository.GetResearches();
+            if(!researches.Success)
+            {
+                throw new NotImplementedException("Error handling for GRPC not implemented yet");
+            }
+            return new Researches
+            {
+                Researches_ = { _mapper.Map<IEnumerable<Research>>(researches.Data) }
+            };
+        }
+
+        public override Task<Research> AddDatasourceToResearch(Datasource request, ServerCallContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task<EmailEmptyResponse> InviteUsersToResearch(EmailRequests request, ServerCallContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task<Research> RemoveDatasourceFromResearch(DatasourceIdRequest request, ServerCallContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
