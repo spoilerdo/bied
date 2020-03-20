29de19d6c6a169066f69771ac8c40fd96ee0147d
nick <n>
2020-03-20 12:31:50 +0100

    Added integration test common library, added integration & unit tests

---
+++ services/research-api/API/research_service/research_service_tests/Collection/InMemoryCollection.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace research_service_tests.Collection
+{
+    [CollectionDefinition("InMemoryCollection")]
+    public class InMemoryCollection : ICollectionFixture<InMemoryTestFixture>
+    {
+    }
+}
