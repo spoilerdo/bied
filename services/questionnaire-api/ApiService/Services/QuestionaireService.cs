364935e605c9ddf74ba004c5feb7ef2ba7520139
ironsm4sh <ironsm4sh@ironsm4sh.nl>
2020-03-23 13:50:25 +0100

    base Questionnaire_Service

---
+++ services/questionnaire-api/ApiService/Services/QuestionaireService.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+
+namespace ApiService
+{
+  public class QuestionnaireService : Questionnaire_Service.Questionnaire_ServiceBase
+  {
+    private readonly ILogger<QuestionnaireService> _logger;
+    public QuestionnaireService(ILogger<QuestionnaireService> logger)
+    {
+      _logger = logger;
+    }
+    public override Task<Questionnaire> CreateQuestionnaire(QuestionnaireCreateRequest request, ServerCallContext context)
+    {
+      // TODO: Validate questionnaire.
+      // TODO: Save validated questionnaire.
+      // TODO: Return the saved questionnaire
+      return Task.FromResult(new Questionnaire
+      {
+        Id = "id",
+        Name = "name",
+        Description = "description"
+      });
+    }
+
+    public override Task<Questionnaire> GetQuestionnaire(QuestionnaireIdRequest request, ServerCallContext context)
+    {
+      // TODO: find questionnaire by id.
+      // TODO: Return the found questionnaire
+      return Task.FromResult(new Questionnaire
+      {
+        Id = "id",
+        Name = "name",
+        Description = "description"
+      });
+    }
+
+    public override Task<Questionnaire> UpdateQuestionnaire(QuestionnaireEditRequest request, ServerCallContext context)
+    {
+      // TODO: Validate questionnaire.
+      // TODO: Update validated questionnaire.
+      // TODO: Return the updated questionnaire
+      return Task.FromResult(new Questionnaire
+      {
+        Id = "id",
+        Name = "name",
+        Description = "description"
+      });
+    }
+
+    public override Task<QuestionnaireEmptyResponse> DeleteQuestionnaire(QuestionnaireIdRequest request, ServerCallContext context)
+    {
+      // TODO: Delete questionnaire.
+      return Task.FromResult(new QuestionnaireEmptyResponse { });
+    }
+  }
+}
