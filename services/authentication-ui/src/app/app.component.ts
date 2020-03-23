358d4fb31501a46024a5f4d85fd5ddfe00e43e77
Oussama El-Hajoui <o.elhajoui@hotmail.nl>
2020-03-23 12:38:16 +0100

    Styled UI

--- services/authentication-ui/src/app/app.component.ts
+++ services/authentication-ui/src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'authentication-ui';
+  title = 'Authentication UI';
 }
+
--- services/authentication-ui/src/app/app.module.ts
+++ services/authentication-ui/src/app/app.module.ts
@@ -4,13 +4,30 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { 
+  NbThemeModule, 
+  NbLayoutModule, 
+  NbButtonModule,
+  NbCardModule,
+  NbInputModule  
+} from '@nebular/theme';
+import { HomeComponent } from './home/home.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+    // Nebular
+    NbThemeModule.forRoot(),
+    NbLayoutModule,
+    NbButtonModule,
+    NbCardModule,
+    NbInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
