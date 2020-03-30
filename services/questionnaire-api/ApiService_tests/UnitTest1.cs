eefa2ab41da432f93bc17afec1a8ddf9a332ee30
Julamm <ironsm4sh@ironsm4sh.nl>
2020-03-30 14:36:28 +0200

    Create test

--- services/questionnaire-api/ApiService_tests/UnitTest1.cs
+++ services/questionnaire-api/ApiService_tests/UnitTest1.cs
@@ -1,15 +1,59 @@
 using System;
+using System.Collections.Generic;
+using AutoMapper;
 using Xunit;
 using Moq;
+using Questionnaire.Persistence.Repositories;
+using Questionnaire.Persistence.Entities;
+using Questionnaire.GRPC;
+using ApiService_tests.Mock;
+using Microsoft.Extensions.Logging;
+using Questionnaire.Services;
+using Grpc.Core;
 
 namespace ApiService_tests
 {
-    public class UnitTest1
+    public class QuestionnaireServiceUnitTest
     {
-        [Fact]
-        public void Test1()
-        {
+        private readonly IQuestionnaireRepository _mockRepository = new MockQuestionnaireRepository();
+        private readonly Mock<ServerCallContext> _mockContext = new Mock<ServerCallContext>();
+        private readonly Mock<ILogger<QuestionnaireService>> _mockLogger = new Mock<ILogger<QuestionnaireService>>();
+        private readonly Mock<IMapper> _mockMapper = new Mock<IMapper>();
+        private readonly QuestionnaireService _questionnaireService;
 
+        public QuestionnaireServiceUnitTest() {
+            _questionnaireService = new QuestionnaireService(_mockLogger.Object, _mockMapper.Object, _mockRepository);
+        }
+
+        [Fact]
+        public async void createQuestionnaire()
+        {
+            // ARRANGE
+            QuestionnaireCreateRequest request = new QuestionnaireCreateRequest{
+             Name = "Request",
+             Description = "request desc"
+            };
+            QuestionnaireEntity entity = new QuestionnaireEntity{
+                ID = "realid",
+                Name = "Request",
+                Description = "request desc",
+                Questions = new List<QuestionEntity>(),
+                ModifiedOn = new DateTime()
+            };
+            QuestionnaireResponse response = new QuestionnaireResponse{
+                Id = "realid",
+                Name = "Request",
+                Description = "request desc"
+            };
+
+            _mockMapper.Setup(x => x.Map<QuestionnaireResponse>(It.IsAny<QuestionnaireEntity>())).Returns(response);
+
+            // ACT
+            QuestionnaireResponse result = await _questionnaireService.CreateQuestionnaire(request, _mockContext.Object);
+
+            //ASSERT
+            Assert.NotNull(result);
+            Assert.Equal("Request", response.Name);
         }
     }
 }
