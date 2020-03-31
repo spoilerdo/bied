352ad472e7ffcb473a2bd6cf92bcbb4e48a0c7b2
MrRikkert <hendrikx.rik@gmail.com>
2020-03-31 11:03:50 +0200

    react on context menu clicks

--- services/research-ui/src/app/questionnaire/pages/questionnaire-overview/questionnaire-card/questionnaire-card.component.ts
+++ services/research-ui/src/app/questionnaire/pages/questionnaire-overview/questionnaire-card/questionnaire-card.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Input, Output } from '@angular/core';
 import { Questionnaire } from 'src/app/models/questionnaire';
-import { NbDialogService } from '@nebular/theme';
+import { NbDialogService, NbMenuService } from '@nebular/theme';
 import { RemoveDialogComponent } from './remove-dialog/remove-dialog.component';
 import { EventEmitter } from '@angular/core';
 import { RenameDialogComponent } from './rename-dialog/rename-dialog.component';
@@ -16,13 +16,25 @@ export class QuestionnaireCardComponent implements OnInit {
   @Output() renameQuestionnaireCallback: EventEmitter<any> = new EventEmitter();
 
   url = '';
-  cardContextItems = [{ title: 'Edit' }, { title: 'View data' }, { title: 'Download data' }];
+  cardContextItems = [{ title: 'Share' }, { title: 'Edit' }, { title: 'View data' }, { title: 'Download data' }];
 
-  constructor(private dialogService: NbDialogService) {}
+  constructor(private dialogService: NbDialogService, private menuService: NbMenuService) {}
 
   ngOnInit(): void {
     // Match URL with real url
     this.url = `/questionnaire/${this.questionnaire.id}`;
+
+    this.menuService.onItemClick().subscribe(event => {
+      if (event.item.title === 'Share') {
+        console.log('share clicked');
+      } else if (event.item.title === 'Edit') {
+        console.log('Edit clicked');
+      } else if (event.item.title === 'View data') {
+        console.log('View data clicked');
+      } else if (event.item.title === 'Download data') {
+        console.log('Download data clicked');
+      }
+    });
   }
 
   openRemoveConfirmation() {
