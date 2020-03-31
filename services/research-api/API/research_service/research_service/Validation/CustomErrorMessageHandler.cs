dcb72e7bc23e7e7ee007e56c22dab73690f126da
nick <n>
2020-03-31 11:18:13 +0200

    WIP : added grpc fluent validator to C# project

---
+++ services/research-api/API/research_service/research_service/Validation/CustomErrorMessageHandler.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation.Results;
+using Grpc.AspNetCore.FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace research_service.Validation
+{
+    public class CustomErrorMessageHandler : IValidatorErrorMessageHandler
+    {
+        public Task<string> HandleAsync(IList<ValidationFailure> failures)
+        {
+            return Task.FromResult(@$"
+            The following validation errors occured: 
+            {string.Join(" - ", failures.Where(f => f.ErrorMessage != null).Select(f => f.ErrorMessage))}");
+        }
+    }
+}
