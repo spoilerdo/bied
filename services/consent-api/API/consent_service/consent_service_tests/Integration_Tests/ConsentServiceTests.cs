fe87684f37742eeaa7856501ef8954f46652901b
Mike Manders <mikemanders1999@gmail.com>
2020-04-02 13:25:28 +0200

    Validation + tests

--- services/consent-api/API/consent_service/consent_service_tests/Integration_Tests/ConsentServiceTests.cs
+++ services/consent-api/API/consent_service/consent_service_tests/Integration_Tests/ConsentServiceTests.cs
@@ -63,6 +63,43 @@ namespace consent_service_tests.Integration_Tests
         }
 
         [Fact]
+        public async void ShouldGetConsentsFailNoUserIdSpecified()
+        {
+            var client = getGrpcClient();
+
+            var createConsent = await client.CreateConsentAsync(
+               new ConsentRequest
+               {
+                   UserId = "631ae791-a25c-4268-a06e-4bb7fe0989aa",
+                   DatasourceId = "ee1b957f-d490-4229-b489-885565cb5b0d",
+                   Consent = true,
+                   Uts = 1585556966
+               }
+           );
+            Assert.NotNull(createConsent);
+
+            var createConsent2 = await client.CreateConsentAsync(
+               new ConsentRequest
+               {
+                   UserId = "631ae791-a25c-4268-a06e-4bb7fe0989aa",
+                   DatasourceId = "ee1b957f-d490-4229-b489-885565cb5b0b",
+                   Consent = true,
+                   Uts = 1585556966
+               }
+           );
+            Assert.NotNull(createConsent2);
+
+            Action act = () =>
+            {
+                client.GetConsentsAsync(new UserIdRequest { Id = "" }).GetAwaiter().GetResult();
+            };
+            Assert.Throws<RpcException>(act);
+
+            var deleteResponse = await client.DeleteAllConsentAsync(new UserIdRequest { Id = "631ae791-a25c-4268-a06e-4bb7fe0989aa" });
+            Assert.NotNull(deleteResponse);
+        }
+
+        [Fact]
         public async void ShouldGetConsent()
         {
             var client = getGrpcClient();
@@ -92,6 +129,19 @@ namespace consent_service_tests.Integration_Tests
             Action act = () =>
             {
                 client.GetConsentAsync(new ConsentIdRequest { ConsentId = "e8802d57-c1b9-4d08-b4b4-bbf3de9ff202" }).GetAwaiter().GetResult();
+            };
+
+            Assert.Throws<RpcException>(act);
+        }
+
+        [Fact]
+        public void ShouldFailToGetConsentsConsentIdNotSpecified()
+        {
+            var client = getGrpcClient();
+
+            Action act = () =>
+            {
+                client.GetConsentAsync(new ConsentIdRequest { ConsentId = "" }).GetAwaiter().GetResult();
             };
 
             Assert.Throws<RpcException>(act);
@@ -129,6 +179,7 @@ namespace consent_service_tests.Integration_Tests
         {
             var client = getGrpcClient();
 
+            // Missing UserId
             Action act = () =>
             {
                 client.CreateConsentAsync(
@@ -142,6 +193,36 @@ namespace consent_service_tests.Integration_Tests
             };
 
             Assert.Throws<RpcException>(act);
+
+            // Missing DatasourceID
+            Action act2 = () =>
+            {
+                client.CreateConsentAsync(
+                    new ConsentRequest
+                    {
+                        UserId = "ee1b957f-d490-4229-b489-885565cb5b0d",
+                        Consent = true,
+                        Uts = 1585556966
+                    }
+                ).GetAwaiter().GetResult();
+            };
+
+            Assert.Throws<RpcException>(act2);
+
+            // Missing Consent
+            Action act3 = () =>
+            {
+                client.CreateConsentAsync(
+                    new ConsentRequest
+                    {
+                        UserId = "ee1b957f-d490-4229-b489-885565cb5b0d",
+                        DatasourceId = "ee1b957f-d490-4229-b489-885565cb5b0d",
+                        Uts = 1585556966
+                    }
+                ).GetAwaiter().GetResult();
+            };
+
+            Assert.Throws<RpcException>(act3);
         }
 
         [Fact]
@@ -176,6 +257,19 @@ namespace consent_service_tests.Integration_Tests
         }
 
         [Fact]
+        public void ShouldFailToDeleteAllConsentsUserIdNotSpecified()
+        {
+            var client = getGrpcClient();
+
+            Action act = () =>
+            {
+                client.DeleteAllConsentAsync(new UserIdRequest { Id = "" }).GetAwaiter().GetResult();
+            };
+
+            Assert.Throws<RpcException>(act);
+        }
+
+        [Fact]
         public async void ShouldDeleteConsent()
         {
             var client = getGrpcClient();
@@ -202,6 +296,18 @@ namespace consent_service_tests.Integration_Tests
             Action act = () =>
             {
                 client.DeleteConsentAsync(new ConsentIdRequest { ConsentId = "631ae791-a25c-4268-a06e-4bb7fe0989aa" }).GetAwaiter().GetResult();
+            };
+
+            Assert.Throws<RpcException>(act);
+        }
+
+        [Fact]
+        public void ShouldFailToDeleteConsentNoConsentIdSpecified()
+        {
+            var client = getGrpcClient();
+            Action act = () =>
+            {
+                client.DeleteConsentAsync(new ConsentIdRequest { ConsentId = "" }).GetAwaiter().GetResult();
             };
 
             Assert.Throws<RpcException>(act);
@@ -258,6 +364,76 @@ namespace consent_service_tests.Integration_Tests
                 }).GetAwaiter().GetResult();
             };
             Assert.Throws<RpcException>(act);
+        }
+
+        [Fact]
+        public void ShouldFailToEditImcompleteConsent()
+        {
+            var client = getGrpcClient();
+
+            // Missing Id
+            Action act = () =>
+            {
+                client.EditConsentAsync(
+                    new ConsentEditRequest
+                    {
+                        UserId = "ee1b957f-d490-4229-b489-885565cb5b0d",
+                        DatasourceId = "ee1b957f-d490-4229-b489-885565cb5b0d",
+                        Consent = true,
+                        Uts = 1585556966
+                    }
+                ).GetAwaiter().GetResult();
+            };
+
+            Assert.Throws<RpcException>(act);
+
+            // Missing UserId
+            Action act2 = () =>
+            {
+                client.EditConsentAsync(
+                     new ConsentEditRequest
+                     {
+                         Id = "ee1b957f-d490-4229-b489-885565cb5b0d",
+                         DatasourceId = "ee1b957f-d490-4229-b489-885565cb5b0d",
+                         Consent = true,
+                         Uts = 1585556966
+                     }
+                ).GetAwaiter().GetResult();
+            };
+
+            Assert.Throws<RpcException>(act2);
+
+            // Missing DatasourceID
+            Action act3 = () =>
+            {
+                client.EditConsentAsync(
+                    new ConsentEditRequest
+                    {
+                        Id = "ee1b957f-d490-4229-b489-885565cb5b0d",
+                        UserId = "ee1b957f-d490-4229-b489-885565cb5b0d",
+                        Consent = true,
+                        Uts = 1585556966
+                    }
+                ).GetAwaiter().GetResult();
+            };
+
+            Assert.Throws<RpcException>(act3);
+
+            // Missing Consent
+            Action act4 = () =>
+            {
+                client.EditConsentAsync(
+                   new ConsentEditRequest
+                   {
+                       Id = "ee1b957f-d490-4229-b489-885565cb5b0d",
+                       UserId = "ee1b957f-d490-4229-b489-885565cb5b0d",
+                       DatasourceId = "ee1b957f-d490-4229-b489-885565cb5b0d",
+                       Uts = 1585556966
+                   }
+                ).GetAwaiter().GetResult();
+            };
+
+            Assert.Throws<RpcException>(act4);
         }
 
         private Consent_Service.Consent_ServiceClient getGrpcClient()
