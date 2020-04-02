9e4830c5d8cce3b605a7cfae34dfc5e790293ca9
Tim van Osch <timvosch@pollex.nl>
2020-04-02 09:47:45 +0200

    Add buttons to header card

--- services/research-ui/src/app/dashboard/dashboard.module.ts
+++ services/research-ui/src/app/dashboard/dashboard.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { DashboardComponent } from './dashboard.component';
 import { HeaderCardComponent } from './header-card/header-card.component';
 import { ThemeModule } from '../@theme/theme.module';
-import { NbCardModule } from '@nebular/theme';
+import { NbCardModule, NbButtonModule } from '@nebular/theme';
 import { CommonModule } from '@angular/common';
 
 @NgModule({
-  imports: [ThemeModule, CommonModule, NbCardModule],
+  imports: [ThemeModule, CommonModule, NbCardModule, NbButtonModule],
   declarations: [DashboardComponent, HeaderCardComponent],
 })
 export class DashboardModule {}
--- services/research-ui/src/app/dashboard/header-card/header-card.component.ts
+++ services/research-ui/src/app/dashboard/header-card/header-card.component.ts
@@ -5,18 +5,21 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./header-card.component.scss'],
   template: `
     <nb-card>
-      <div class="icon-container">
-        <div class="icon">
-          <ng-content></ng-content>
-        </div>
-      </div>
-
       <div class="details">
+        <div class="icon-container">
+          <div class="icon">
+            <img src="{{ icon }}" alt="" />
+          </div>
+        </div>
         <div class="title h5">{{ title }}</div>
+      </div>
+      <div class="button-container">
+        <ng-content></ng-content>
       </div>
     </nb-card>
   `,
 })
 export class HeaderCardComponent {
   @Input() title: string;
+  @Input() icon: string;
 }
