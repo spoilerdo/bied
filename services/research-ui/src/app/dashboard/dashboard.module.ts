4b1b41ca82f93f9f618e8342413f5b850e32d770
Tim van Osch <timvosch@pollex.nl>
2020-04-02 11:21:24 +0200

    Add research-list component

--- services/research-ui/src/app/dashboard/dashboard.module.ts
+++ services/research-ui/src/app/dashboard/dashboard.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { DashboardComponent } from './dashboard.component';
 import { HeaderCardComponent } from './header-card/header-card.component';
 import { ThemeModule } from '../@theme/theme.module';
-import { NbCardModule, NbButtonModule } from '@nebular/theme';
+import { NbCardModule, NbButtonModule, NbListModule } from '@nebular/theme';
 import { CommonModule } from '@angular/common';
+import { ResearchListComponent } from './research-list/research-list.component';
 
 @NgModule({
-  imports: [ThemeModule, CommonModule, NbCardModule, NbButtonModule],
-  declarations: [DashboardComponent, HeaderCardComponent],
+  imports: [ThemeModule, CommonModule, NbCardModule, NbButtonModule, NbListModule],
+  declarations: [DashboardComponent, HeaderCardComponent, ResearchListComponent],
 })
 export class DashboardModule {}
---
+++ services/research-ui/src/app/dashboard/research-list/research-list.component.ts
@@ -0,0 +1,8 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'ngx-research-list',
+  styleUrls: ['./research-list.component.scss'],
+  templateUrl: './research-list.component.html',
+})
+export class ResearchListComponent {}
