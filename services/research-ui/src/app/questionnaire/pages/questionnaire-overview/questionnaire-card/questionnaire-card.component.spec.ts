f081f4a688c2c976b011c3015075817c9fe19b62
MrRikkert <hendrikx.rik@gmail.com>
2020-04-14 13:29:20 +0200

    added questionnaire card component tests

--- services/research-ui/src/app/questionnaire/pages/questionnaire-overview/questionnaire-card/questionnaire-card.component.spec.ts
+++ services/research-ui/src/app/questionnaire/pages/questionnaire-overview/questionnaire-card/questionnaire-card.component.spec.ts
@@ -12,10 +12,14 @@ import {
 } from '@nebular/theme';
 import { TruncatePipe } from 'src/app/questionnaire/utils/TruncatePipe';
 import { NbEvaIconsModule } from '@nebular/eva-icons';
+import { By } from '@angular/platform-browser';
 
 describe('QuestionnaireCardComponent', () => {
   let component: QuestionnaireCardComponent;
   let fixture: ComponentFixture<QuestionnaireCardComponent>;
+  let mockDialogService = {
+    open: () => {},
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -28,7 +32,7 @@ describe('QuestionnaireCardComponent', () => {
         NbIconModule,
         NbEvaIconsModule,
       ],
-      providers: [{ provide: NbDialogService, useValue: {} }, NbMenuService],
+      providers: [{ provide: NbDialogService, useValue: mockDialogService }, NbMenuService],
     }).compileComponents();
   }));
 
@@ -45,5 +49,29 @@ describe('QuestionnaireCardComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call openRenameDialog', () => {
+    spyOn(component, 'openRenameDialog');
+    fixture.debugElement.query(By.css('#questionnaire-card-rename')).triggerEventHandler('click', null);
+    expect(component.openRenameDialog).toHaveBeenCalled();
+  });
+
+  it('should call openRemoveConfirmation', () => {
+    spyOn(component, 'openRemoveConfirmation');
+    fixture.debugElement.query(By.css('#questionnaire-card-remove')).triggerEventHandler('click', null);
+    expect(component.openRemoveConfirmation).toHaveBeenCalled();
+  });
+
+  it('should call duplicateQuestionnaire', () => {
+    spyOn(component, 'duplicateQuestionnaire');
+    fixture.debugElement.query(By.css('#questionnaire-card-copy')).triggerEventHandler('click', null);
+    expect(component.duplicateQuestionnaire).toHaveBeenCalled();
+  });
+
+  it('should call openCardpopup', () => {
+    spyOn(component, 'openCardPopup');
+    fixture.debugElement.query(By.css('#questionnaire-card-open')).triggerEventHandler('click', null);
+    expect(component.openCardPopup).toHaveBeenCalled();
   });
 });
