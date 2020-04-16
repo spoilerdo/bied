f76fbe272e76ab014c55ac7cb50d377854558ead
MrRikkert <hendrikx.rik@gmail.com>
2020-04-16 09:47:23 +0200

    added tests for rename-dialog

--- services/research-ui/src/app/questionnaire/pages/questionnaire-overview/questionnaire-card/rename-dialog/rename-dialog.component.spec.ts
+++ services/research-ui/src/app/questionnaire/pages/questionnaire-overview/questionnaire-card/rename-dialog/rename-dialog.component.spec.ts
@@ -3,10 +3,14 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RenameDialogComponent } from './rename-dialog.component';
 import { NbDialogRef, NbCardModule, NbInputModule, NbThemeModule } from '@nebular/theme';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
 
 describe('RenameDialogComponent', () => {
   let component: RenameDialogComponent;
   let fixture: ComponentFixture<RenameDialogComponent>;
+  const mockDialogRef = {
+    open: (res?: any) => {},
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -29,5 +33,42 @@ describe('RenameDialogComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('cancel button', () => {
+    it('should call close()', () => {
+      spyOn(component, 'close');
+      fixture.debugElement.query(By.css('#rename-dialog-cancel')).triggerEventHandler('click', null);
+      expect(component.close).toHaveBeenCalled();
+    });
+  });
+
+  describe('rename button', () => {
+    it('should call rename()', () => {
+      spyOn(component, 'rename');
+      fixture.debugElement.query(By.css('#rename-dialog-rename')).triggerEventHandler('click', null);
+      expect(component.rename).toHaveBeenCalled();
+    });
+
+    it('should be disabled, empty values', () => {
+      component.oldTitle = null;
+      component.newTitle = null;
+      fixture.detectChanges();
+      expect(fixture.debugElement.nativeElement.querySelector('#rename-dialog-rename').disabled).toBeTruthy();
+    });
+
+    it('should be disabled, short new name', () => {
+      component.oldTitle = 'test';
+      component.newTitle = 'new';
+      fixture.detectChanges();
+      expect(fixture.debugElement.nativeElement.querySelector('#rename-dialog-rename').disabled).toBeTruthy();
+    });
+
+    it('should be disabled, wrong old name', () => {
+      component.oldTitle = 'tests';
+      component.newTitle = 'newname';
+      fixture.detectChanges();
+      expect(fixture.debugElement.nativeElement.querySelector('#rename-dialog-rename').disabled).toBeTruthy();
+    });
   });
 });
