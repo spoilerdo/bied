1b49724f6adeb3e18d34d35315eea0b586161e71
MrRikkert <hendrikx.rik@gmail.com>
2020-04-16 09:53:17 +0200

    added remove-dialog tests

--- services/research-ui/src/app/questionnaire/pages/questionnaire-overview/questionnaire-card/remove-dialog/remove-dialog.component.spec.ts
+++ services/research-ui/src/app/questionnaire/pages/questionnaire-overview/questionnaire-card/remove-dialog/remove-dialog.component.spec.ts
@@ -3,6 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RemoveDialogComponent } from './remove-dialog.component';
 import { NbDialogRef, NbCardModule } from '@nebular/theme';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
 
 describe('RemoveDialogComponent', () => {
   let component: RemoveDialogComponent;
@@ -30,5 +31,39 @@ describe('RemoveDialogComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('cancel button', () => {
+    it('should call close()', () => {
+      spyOn(component, 'close');
+      fixture.debugElement.query(By.css('#remove-dialog-cancel')).triggerEventHandler('click', null);
+      expect(component.close).toHaveBeenCalled();
+    });
+  });
+
+  describe('rename button', () => {
+    it('should call rename()', () => {
+      spyOn(component, 'remove');
+      fixture.debugElement.query(By.css('#remove-dialog-remove')).triggerEventHandler('click', null);
+      expect(component.remove).toHaveBeenCalled();
+    });
+
+    it('should be disabled, empty values', () => {
+      component.typedTitle = null;
+      fixture.detectChanges();
+      expect(fixture.debugElement.nativeElement.querySelector('#remove-dialog-remove').disabled).toBeTruthy();
+    });
+
+    it('should be disabled, wrong old title', () => {
+      component.typedTitle = 'wrong title';
+      fixture.detectChanges();
+      expect(fixture.debugElement.nativeElement.querySelector('#remove-dialog-remove').disabled).toBeTruthy();
+    });
+
+    it('should be enabled', () => {
+      component.typedTitle = 'test';
+      fixture.detectChanges();
+      expect(fixture.debugElement.nativeElement.querySelector('#remove-dialog-remove').disabled).toBeFalsy();
+    });
   });
 });
