e3cb8ee1850c7aa6e5b1460ed991046b3bdf8f57
mikemanders <36154936+mikemanders@users.noreply.github.com>
2020-05-15 19:56:56 +0200

    Revert "Feature/consent ui"

--- services/consent-ui/src/app/@theme/theme.module.ts
+++
@@ -1,7 +0,0 @@
-import { NgModule, ModuleWithProviders } from '@angular/core';
-import { NbThemeModule, NbLayoutModule, NbCardModule } from '@nebular/theme';
-@NgModule({
-  imports: [NbThemeModule.forRoot()],
-  exports: [],
-})
-export class ThemeModule {}
--- services/consent-ui/src/app/providers/user.provider.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from "@angular/core";
-
-@Injectable()
-export class UserProvider {
-  getUserById(id: string): any {
-    throw new Error('Method not implemented')
-  }
-}
