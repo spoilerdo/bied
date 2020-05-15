34ff5b7ddc568b934225d16f17d38e7b2c54fa8a
Mike Manders <mikemanders1999@gmail.com>
2020-05-15 12:08:04 +0200

    Fixed the popover, wasn't opening after viewing another page

--- services/consent-ui/e2e/src/app/consents-list-item/consents-list-item.po.ts
+++ services/consent-ui/e2e/src/app/consents-list-item/consents-list-item.po.ts
@@ -8,7 +8,7 @@ export class ConsentsListItem {
   getMoreOptionsButton() {
     return element(
       by.xpath(
-        '/html/body/app-root/nb-layout/div[1]/div/div/div/div/nb-layout-column/app-consents/nb-card/app-consents-list/nb-list/nb-list-item[1]/app-consents-list-item/div/nb-icon'
+        '//*[@id="consent-more-options"]'
       )
     )
   }
